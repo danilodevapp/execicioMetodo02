33b30e41f186c43967bb8e2f636daafe80e31d0b
danilodevapp <danilodevapp@gmail.com>
2024-04-17 14:05:24 -0300

    exec02

---
+++ src/App.java
@@ -0,0 +1,31 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Segundo exercício sobre métodos!");
+        String primeiroNome = "Danilo ";
+        String segundoNome = "Estevão";
+        String nomeSobreNome = juntaNome(primeiroNome, segundoNome);
+        String ultimoNome = " Cordeiro";
+        String nomeCompleto = juntaUltimoNome(ultimoNome);
+        System.out.print(nomeSobreNome);
+        System.out.println(juntaUltimoNome(ultimoNome));
+
+        int num1 = 10;
+        int num2 = 10;
+        int soma = somar(num1, num2);
+
+        System.out.println(somar(num1, num2));
+    }
+
+    public static String juntaNome (String primeiroNome, String segundoNome){
+        return primeiroNome.concat(segundoNome);
+    }
+
+    public static String juntaUltimoNome (String ultimoNome){
+        return ultimoNome;
+    }
+
+    public static int somar (int num1, int num2){
+        return num1 + num2;
+    }
+}
